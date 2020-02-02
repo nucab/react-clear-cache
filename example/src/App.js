c86751819c8b13fb7b7019c671726f9629ec2ea8
Noah John Ucab <noahjohn.ucab@gmail.com>
2020-02-02 23:57:52 +0800

    feat(example): update component

--- example/src/App.js
+++ example/src/App.js
@@ -1,42 +1,39 @@
 import * as React from 'react';
-import ClearCache from 'react-clear-cache';
+import { useClearCache } from 'react-clear-cache';
 
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
+  const { isLatestVersion, emptyCacheStorage } = useClearCache({ duration: 5000 });
   return (
-    <ClearCache duration={5000}>
-      {({ isLatestVersion, latestVersion, emptyCacheStorage }) => (
-        <div className="App">
-          <header className="App-header">
-            <p>
-              <img src={logo} className="App-logo" alt="logo" />
-            </p>
-            <p>
-              <strong>Is latest version</strong>:{' '}
-              {isLatestVersion ? 'Yes' : 'No'}
-            </p>
-            {!isLatestVersion && (
-              <p>
-                <a
-                  href="#"
-                  onClick={e => {
-                    e.preventDefault();
-                    emptyCacheStorage();
-                  }}
-                >
-                  Update version
-                </a>
-              </p>
-            )}
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-          </header>
-        </div>
-      )}
-    </ClearCache>
+    <div className="App">
+      <header className="App-header">
+        <p>
+          <img src={logo} className="App-logo" alt="logo" />
+        </p>
+        <p>
+          <strong>Is latest version</strong>:{' '}
+          {isLatestVersion ? 'Yes' : 'No'}
+        </p>
+        {!isLatestVersion && (
+          <p>
+            <a
+              href="#"
+              onClick={e => {
+                e.preventDefault();
+                emptyCacheStorage();
+              }}
+            >
+              Update version
+            </a>
+          </p>
+        )}
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
   );
 };
 
