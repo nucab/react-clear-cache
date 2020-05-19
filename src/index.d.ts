6ab8cb5f19335f57af165e2d148daaa101549401
Federico Babrauskas <fedebabrauskas@gmail.com>
2020-05-19 17:15:45 -0300

    Added support for base path

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,8 +1,18 @@
 import * as React from 'react';
+declare const defaultProps: {
+    duration: number;
+    auto: boolean;
+    storageKey: string;
+    basePath: string;
+};
 declare type OwnProps = {
-    duration?: number;
-    auto?: boolean;
-    children: any;
+    children?: any;
+} & typeof defaultProps;
+export declare const useClearCache: (props: OwnProps) => {
+    loading: boolean;
+    isLatestVersion: boolean;
+    emptyCacheStorage: (version: string) => Promise<void>;
+    latestVersion: string;
 };
 declare const ClearCache: React.FC<OwnProps>;
 export default ClearCache;
