7ce0a89cc36a0fb3a641d21a25f7fb075d6b414d
Khánh Hoàng <6380436+hckhanh@users.noreply.github.com>
2022-06-26 08:49:11 +0700

    Remove unused `rollup-plugin-postcss`

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
 import url from '@rollup/plugin-url';
 
@@ -25,9 +24,6 @@ export default {
   ],
   plugins: [
     external(),
-    postcss({
-      modules: true,
-    }),
     url(),
     resolve(),
     typescript({
